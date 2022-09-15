0438820395695172879ad86785de02148019dcec
hassan mohamed kamel <hassankamel755@gmail.com>
2022-09-15 02:59:50 +0200

    first commit

--- src/component/Details.js
+++ src/component/Details.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Container, Grid, Typography, Button } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
